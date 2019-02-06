9e26932a4a87ca7e1a58f82c971e70be9a10f574
fonsms <alfonso.mosanz@alumnos.upm.es>
2019-02-06 01:02:11 +0100

    libreria

---
+++ Library/DriverLed.cpp
@@ -0,0 +1,15 @@
+#include "Arduino.h"
+#include "DriverLed.h"
+
+
+DriverLed :: DriverLed (int entradaaaaa) // constructor
+{
+
+}
+
+void DriverLed:: funcion( ) 
+{
+}
+void DriverLed:: funcion1( ) 
+{
+}
---
+++ Library/DriverLed.h
@@ -0,0 +1,13 @@
+#ifndef DriverLed_h
+#define DriverLed_h
+#include "Arduino.h" 
+clase DriverLed
+{
+ publico :
+ DriverLed ( int entradaaaa) ; // constructor
+ void funcion();
+ void funcion2();
+  privado :
+int entradaaaaa 
+};
+#endif
